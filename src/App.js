823f252ba41f6e7b262955321af05dbeebafb2d6
Daksh Dadhania <dakshdadhania@gmail.com>
2022-12-23 16:13:13 +0530

    aboutus link and spacing

--- src/App.js
+++ src/App.js
@@ -46,13 +46,14 @@ function App() {
           <div className="content">
             <p>niteCTF is a jeopardy style CTF for students interested in cybersecurity. niteCTF is designed to help students explore newer domains in cybersecurity as well as help existing professionals practise their skills. We will feature challenges which cover various domains of cybersecurity including cryptography, reversing, forensics, web exploitation, pwn and more!
             </p>
-            <p>We will release the registration forms soon!</p>
+            <p>Registrations are open!</p>
             {/* <p>1st place - 8000 INR<br/>2nd place - 5000 INR<br/>3rd place - 3000 INR</p> */}
             <p><h3><Timer/> </h3></p>
             {/* <p><small>Infra sponsored by <a href="https://g.co/cloud"><img className="content-google-cloud-logo" src={gcpLogo} alt="Google Cloud"></img></a></small></p> */}
             <p>
               <a href="https://discord.gg/G2T2yWzK" class="discord-button">Join Our Discord</a>&nbsp;
               <a href="https://nite-trial12022.el.r.appspot.com/login" class="event-button">Event Page</a> <br />&nbsp;
+              <br />&nbsp;&nbsp;&nbsp;&nbsp;
               {/* <a href="https://discord.gg/thTagHkyWB" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faDiscord}/></a>&nbsp; */}
               <a href="https://www.facebook.com/cryptonitemanipal/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faFacebook}/></a>&nbsp;
               <a href="https://www.instagram.com/cryptonite_mit/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram}/></a>&nbsp;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 function Header() {
   return <header className="header">
     <ul>
-      <li><a className="hover-underline-animation" href="https://cryptonite.team" target="_blank" rel="noreferrer">About us</a></li>
+      <li><a className="hover-underline-animation" href="https://cryptonitemit.com" target="_blank" rel="noreferrer">About us</a></li>
       {/* <li><a className="hover-underline-animation" href="https://ctf.cryptonite.team/teams" target="_blank" rel="noreferrer">Teams</a></li> */}
       {/* <li><a className="hover-underline-animation" href="https://ctf.cryptonite.team/scoreboard" target="_blank" rel="noreferrer">Scoreboard</a></li> */}
       {/* <li><a className="hover-underline-animation" href="https://ctf.cryptonite.team/notifications" target="_blank" rel="noreferrer">Notifications</a></li> */}
